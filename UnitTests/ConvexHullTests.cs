298b0fef445cd69bf9d729d8bdfe4fc2f50ea8a6
Pavel Figurin <pavel_figurin@epam.com>
2023-03-16 17:53:57 +0300

    convex hull test cases

--- UnitTests/ConvexHullTests.cs
+++ UnitTests/ConvexHullTests.cs
@@ -11,14 +11,22 @@ namespace UnitTests
 {
     class ConvexHullTests
     {
-        [Test]
-        public void TestConvexHull()
-        {
-            var points = new List<Point>() { new Point(0, 0), new Point(1, 0),
-                                             new Point(0, 1), new Point(1, 1)};
+        private List<Point> points = new List<Point>() { new Point(0, 0),                                   new Point(2, 0),
+                                                                          new Point(1, 1), new Point(2, 1),                  new Point(3, 1),
 
-            var result = ConvexHullHelper.GetPolygon(points);
-            Assert.IsTrue(result.All(p => points.Contains(p)));
+                                                                          new Point(1, 3),                                   new Point(3, 3)};
+
+        [TestCase(0, true)]
+        [TestCase(1, true)]
+        [TestCase(2, false)]
+        [TestCase(3, false)]
+        [TestCase(4, true)]
+        [TestCase(5, true)]
+        [TestCase(6, true)]
+        public void TestConvexHull(int pointId, bool isPartOfTheHull)
+        {
+            var hull = ConvexHullHelper.GetPolygon(points);
+            Assert.AreEqual(hull.Contains(points[pointId]), isPartOfTheHull);
         }
     }
 }
